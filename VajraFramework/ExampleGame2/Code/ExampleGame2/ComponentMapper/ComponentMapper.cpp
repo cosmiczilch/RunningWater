eb6a981fed08c488824905092fb2f5847ca6bc84
cosmiczilch <cosmiczilch@gmail.com>
2014-02-14 01:11:21 -0500

    A lot more cleanup. Deleting anything that is specific to ExampleGame.

--- VajraFramework/ExampleGame2/Code/ExampleGame2/ComponentMapper/ComponentMapper.cpp
+++ VajraFramework/ExampleGame2/Code/ExampleGame2/ComponentMapper/ComponentMapper.cpp
@@ -11,20 +11,7 @@
 #include "Vajra/Engine/Components/DerivedComponents/Armature/Armature.h"
 #include "Vajra/Engine/Components/DerivedComponents/Transform/Transform.h"
 #include "Vajra/Engine/ParticleSystems/ParticleSystem.h"
-#include "ExampleGame2/Components/LevelManager/LevelManager.h"
 #include "ExampleGame2/Components/GameScripts/SampleGameScript.h"
-#include "ExampleGame2/Components/GameScripts/UnitAnimations/UnitAnimationManager.h"
-#include "ExampleGame2/Components/GameScripts/Ai/AiPerception.h"
-#include "ExampleGame2/Components/GameScripts/Ai/AiRoutine.h"
-#include "ExampleGame2/Components/GameScripts/Ai/AiKnowledge.h"
-#include "ExampleGame2/Components/GameScripts/Units/EnemyUnit.h"
-#include "ExampleGame2/Components/GameScripts/Units/Thief.h"
-#include "ExampleGame2/Components/GameScripts/Units/Assassin.h"
-#include "ExampleGame2/Components/ShadyCamera/ShadyCamera.h"
-#include "ExampleGame2/Components/Grid/GridManager.h"
-#include "ExampleGame2/Components/Grid/GridZone.h"
-#include "ExampleGame2/Components/Grid/GridNavigator.h"
-#include "ExampleGame2/Components/Grid/GridZones/TransitionZone.h"
 
 
 Component* ComponentMapper::AddNewComponentToGameObjectByComponentName(GameObject* gameObject, std::string componentName) {
@@ -90,87 +77,9 @@ Component* ComponentMapper::AddNewComponentToGameObjectByComponentName(GameObjec
 		return component;
 	}
 	
-	if (componentName == "LevelManager") {
-		LevelManager* component = gameObject->GetComponent<LevelManager>();
-		if (component == nullptr) { component = gameObject->AddComponent<LevelManager>(); }
-		return component;
-	}
-	
 	if (componentName == "SampleGameScript") {
 		SampleGameScript* component = gameObject->GetComponent<SampleGameScript>();
 		if (component == nullptr) { component = gameObject->AddComponent<SampleGameScript>(); }
-		return component;
-	}
-	
-	if (componentName == "UnitAnimationManager") {
-		UnitAnimationManager* component = gameObject->GetComponent<UnitAnimationManager>();
-		if (component == nullptr) { component = gameObject->AddComponent<UnitAnimationManager>(); }
-		return component;
-	}
-	
-	if (componentName == "AiPerception") {
-		AiPerception* component = gameObject->GetComponent<AiPerception>();
-		if (component == nullptr) { component = gameObject->AddComponent<AiPerception>(); }
-		return component;
-	}
-	
-	if (componentName == "AiRoutine") {
-		AiRoutine* component = gameObject->GetComponent<AiRoutine>();
-		if (component == nullptr) { component = gameObject->AddComponent<AiRoutine>(); }
-		return component;
-	}
-	
-	if (componentName == "AiKnowledge") {
-		AiKnowledge* component = gameObject->GetComponent<AiKnowledge>();
-		if (component == nullptr) { component = gameObject->AddComponent<AiKnowledge>(); }
-		return component;
-	}
-	
-	if (componentName == "EnemyUnit") {
-		EnemyUnit* component = gameObject->GetComponent<EnemyUnit>();
-		if (component == nullptr) { component = gameObject->AddComponent<EnemyUnit>(); }
-		return component;
-	}
-	
-	if (componentName == "Thief") {
-		Thief* component = gameObject->GetComponent<Thief>();
-		if (component == nullptr) { component = gameObject->AddComponent<Thief>(); }
-		return component;
-	}
-	
-	if (componentName == "Assassin") {
-		Assassin* component = gameObject->GetComponent<Assassin>();
-		if (component == nullptr) { component = gameObject->AddComponent<Assassin>(); }
-		return component;
-	}
-	
-	if (componentName == "ShadyCamera") {
-		ShadyCamera* component = gameObject->GetComponent<ShadyCamera>();
-		if (component == nullptr) { component = gameObject->AddComponent<ShadyCamera>(); }
-		return component;
-	}
-	
-	if (componentName == "GridManager") {
-		GridManager* component = gameObject->GetComponent<GridManager>();
-		if (component == nullptr) { component = gameObject->AddComponent<GridManager>(); }
-		return component;
-	}
-	
-	if (componentName == "GridZone") {
-		GridZone* component = gameObject->GetComponent<GridZone>();
-		if (component == nullptr) { component = gameObject->AddComponent<GridZone>(); }
-		return component;
-	}
-	
-	if (componentName == "GridNavigator") {
-		GridNavigator* component = gameObject->GetComponent<GridNavigator>();
-		if (component == nullptr) { component = gameObject->AddComponent<GridNavigator>(); }
-		return component;
-	}
-	
-	if (componentName == "TransitionZone") {
-		TransitionZone* component = gameObject->GetComponent<TransitionZone>();
-		if (component == nullptr) { component = gameObject->AddComponent<TransitionZone>(); }
 		return component;
 	}
 
@@ -426,167 +335,9 @@ void ComponentMapper::InitializePropertyByComponentAndPropertyNames(GameObject *
 		return;
 	}
 	
-	if (componentName == "LevelManager") {
-		LevelManager* component = gameObject->GetComponent<LevelManager>();
-		if (component == nullptr) { return; }
-		if (propertyName == "LoadLevelFromFile") {
-			if ((int)argv.size() < 1) { return; }
-			component->LoadLevelFromFile(ConvertStringToString(argv[0]));
-			return;
-		}
-		return;
-	}
-	
 	if (componentName == "SampleGameScript") {
 		SampleGameScript* component = gameObject->GetComponent<SampleGameScript>();
 		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "UnitAnimationManager") {
-		UnitAnimationManager* component = gameObject->GetComponent<UnitAnimationManager>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "AiPerception") {
-		AiPerception* component = gameObject->GetComponent<AiPerception>();
-		if (component == nullptr) { return; }
-		if (propertyName == "SetVisionRange") {
-			if ((int)argv.size() < 1) { return; }
-			component->SetVisionRange(StringUtilities::ConvertStringToFloat(argv[0]));
-			return;
-		}
-		if (propertyName == "SetFieldOfVision") {
-			if ((int)argv.size() < 1) { return; }
-			component->SetFieldOfVision(StringUtilities::ConvertStringToFloat(argv[0]));
-			return;
-		}
-		if (propertyName == "SetFieldOfVisionDegrees") {
-			if ((int)argv.size() < 1) { return; }
-			component->SetFieldOfVisionDegrees(StringUtilities::ConvertStringToFloat(argv[0]));
-			return;
-		}
-		if (propertyName == "SetVisionAcuity") {
-			if ((int)argv.size() < 1) { return; }
-			component->SetVisionAcuity(StringUtilities::ConvertStringToFloat(argv[0]));
-			return;
-		}
-		return;
-	}
-	
-	if (componentName == "AiRoutine") {
-		AiRoutine* component = gameObject->GetComponent<AiRoutine>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "AiKnowledge") {
-		AiKnowledge* component = gameObject->GetComponent<AiKnowledge>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "EnemyUnit") {
-		EnemyUnit* component = gameObject->GetComponent<EnemyUnit>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "Thief") {
-		Thief* component = gameObject->GetComponent<Thief>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "Assassin") {
-		Assassin* component = gameObject->GetComponent<Assassin>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "ShadyCamera") {
-		ShadyCamera* component = gameObject->GetComponent<ShadyCamera>();
-		if (component == nullptr) { return; }
-		if (propertyName == "MoveTo") {
-			if ((int)argv.size() < 3) { return; }
-			component->MoveTo(StringUtilities::ConvertStringToFloat(argv[0]), StringUtilities::ConvertStringToFloat(argv[1]), StringUtilities::ConvertStringToFloat(argv[2]));
-			return;
-		}
-		if (propertyName == "MoveToRoom") {
-			if ((int)argv.size() < 2) { return; }
-			component->MoveToRoom(StringUtilities::ConvertStringToFloat(argv[0]), StringUtilities::ConvertStringToFloat(argv[1]));
-			return;
-		}
-		if (propertyName == "MoveToOverview") {
-			if ((int)argv.size() < 0) { return; }
-			component->MoveToOverview();
-			return;
-		}
-		return;
-	}
-	
-	if (componentName == "GridManager") {
-		GridManager* component = gameObject->GetComponent<GridManager>();
-		if (component == nullptr) { return; }
-		return;
-	}
-	
-	if (componentName == "GridZone") {
-		GridZone* component = gameObject->GetComponent<GridZone>();
-		if (component == nullptr) { return; }
-		if (propertyName == "SetZoneBounds") {
-			if ((int)argv.size() < 4) { return; }
-			component->SetZoneBounds(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]), StringUtilities::ConvertStringToInt(argv[2]), StringUtilities::ConvertStringToInt(argv[3]));
-			return;
-		}
-		return;
-	}
-	
-	if (componentName == "GridNavigator") {
-		GridNavigator* component = gameObject->GetComponent<GridNavigator>();
-		if (component == nullptr) { return; }
-		if (propertyName == "SetMovementSpeed") {
-			if ((int)argv.size() < 1) { return; }
-			component->SetMovementSpeed(StringUtilities::ConvertStringToFloat(argv[0]));
-			return;
-		}
-		if (propertyName == "SetGridPosition") {
-			if ((int)argv.size() < 2) { return; }
-			component->SetGridPosition(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]));
-			return;
-		}
-		if (propertyName == "SetDestination") {
-			if ((int)argv.size() < 2) { return; }
-			component->SetDestination(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]));
-			return;
-		}
-		if (propertyName == "AddDestination") {
-			if ((int)argv.size() < 2) { return; }
-			component->AddDestination(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]));
-			return;
-		}
-		return;
-	}
-	
-	if (componentName == "TransitionZone") {
-		TransitionZone* component = gameObject->GetComponent<TransitionZone>();
-		if (component == nullptr) { return; }
-		if (propertyName == "SetFirstDestination") {
-			if ((int)argv.size() < 2) { return; }
-			component->SetFirstDestination(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]));
-			return;
-		}
-		if (propertyName == "SetSecondDestination") {
-			if ((int)argv.size() < 2) { return; }
-			component->SetSecondDestination(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]));
-			return;
-		}
-		if (propertyName == "SetDestinations") {
-			if ((int)argv.size() < 4) { return; }
-			component->SetDestinations(StringUtilities::ConvertStringToInt(argv[0]), StringUtilities::ConvertStringToInt(argv[1]), StringUtilities::ConvertStringToInt(argv[2]), StringUtilities::ConvertStringToInt(argv[3]));
-			return;
-		}
 		return;
 	}
 }
--- VajraFramework/ExampleGame2/Code/ExampleGame2/Components/Grid/GridConstants.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  GridConstants.cpp
-//  Created by Matt Kaufmann on 02/03/14.
-//
-
-#include "ExampleGame2/Components/Grid/GridConstants.h"
-
-const glm::vec3 HALF_CELL = glm::vec3(CELL_SIZE / 2.0f, GROUND_Y, -CELL_SIZE / 2.0f);
